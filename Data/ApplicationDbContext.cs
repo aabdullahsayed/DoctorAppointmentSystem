32d0322f906284134389142d120adcdc18f0a664
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-02-19 23:36:37 +0600

    implement ApplicationDbContext and register DbSets

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HospitalManagementSystem.Models;
+
+namespace HospitalManagementSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<DoctorSchedule> DoctorSchedules { get; set; }
+        
+        
+    }
+}
